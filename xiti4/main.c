1d7e2da351dda52594e059b3cc513aed0db62c33
中软mini025 <zhongruanmini025@zhongruanmini025deMac-mini.local>
2015-07-28 17:35:59 -0700

    Initial Commit

---
+++ xiti4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  xiti4
+//
+//  Created by 中软mini025 on 15/7/28.
+//  Copyright (c) 2015年 中软mini025. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
